c680eff4f9a868cbc30f25a8050f1d3c5c100365
Jeannie <fareldwianggara05@gmail.com>
2026-05-15 03:19:11 +0000

    FIX: KSM ERROR.

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -24,13 +24,13 @@
 #include "internal.h"
 
 /* gross hack for <=4.19 stable */
-#if defined(CONFIG_S390) || defined(CONFIG_ARM)
+#if defined(CONFIG_S390) || defined(CONFIG_ARM) || defined(CONFIG_ARM64)
 static void tlb_remove_table_smp_sync(void *arg)
 {
         /* Simply deliver the interrupt */
 }
 
-static void tlb_remove_table_sync_one(void)
+void tlb_remove_table_sync_one(void)
 {
         smp_call_function(tlb_remove_table_smp_sync, NULL, 1);
 }
