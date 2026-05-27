321a1286ebb84adc3604c99a345cda4556efd665
Jeannie <fareldwianggara05@gmail.com>
2026-05-27 01:33:29 +0000

    Tune: EAS ksm and tcp.

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -104,7 +104,7 @@ void tcp_time_wait(struct sock *sk, int state, int timeo);
 				 * 15 is ~13-30min depending on RTO.
 				 */
 
-#define TCP_SYN_RETRIES	 6	/* This is how many retries are done
+#define TCP_SYN_RETRIES	 4	/* This is how many retries are done
 				 * when active opening a connection.
 				 * RFC1122 says the minimum retry MUST
 				 * be at least 180secs.  Nevertheless
@@ -155,9 +155,9 @@ void tcp_time_wait(struct sock *sk, int state, int timeo);
 #define TCP_RESOURCE_PROBE_INTERVAL ((unsigned)(HZ/2U)) /* Maximal interval between probes
 					                 * for local resources.
 					                 */
-#define TCP_KEEPALIVE_TIME	(120*60*HZ)	/* two hours */
-#define TCP_KEEPALIVE_PROBES	9		/* Max of 9 keepalive probes	*/
-#define TCP_KEEPALIVE_INTVL	(75*HZ)
+#define TCP_KEEPALIVE_TIME	(10*60*HZ)	/* two hours */
+#define TCP_KEEPALIVE_PROBES	5		/* Max of 9 keepalive probes	*/
+#define TCP_KEEPALIVE_INTVL	(30*HZ)
 
 #define MAX_TCP_KEEPIDLE	32767
 #define MAX_TCP_KEEPINTVL	32767
