0fa5e9a2212304fe4942b76d2e3010e5b0b5190d
Jeannie <fareldwianggara05@gmail.com>
2026-06-02 14:59:06 +0000

    Revert "sched/fair: post_init_entity_util_avg cap 50%->75%"
    This reverts commit 5c892a678021d9a17c88d342233212f4c62b992d.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -802,7 +802,7 @@ void post_init_entity_util_avg(struct sched_entity *se)
 	struct cfs_rq *cfs_rq = cfs_rq_of(se);
 	struct sched_avg *sa = &se->avg;
 	long cpu_scale = arch_scale_cpu_capacity(NULL, cpu_of(rq_of(cfs_rq)));
-	long cap = (long)(cpu_scale - cfs_rq->avg.util_avg) * 3 / 4; /* KernelBumi: 75% init util */
+	long cap = (long)(cpu_scale - cfs_rq->avg.util_avg) / 2;
 
 	if (cap > 0) {
 		if (cfs_rq->avg.util_avg != 0) {
