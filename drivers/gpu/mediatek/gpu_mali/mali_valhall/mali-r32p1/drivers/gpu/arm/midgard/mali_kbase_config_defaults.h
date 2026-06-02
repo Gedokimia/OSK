c9a5530732a9402bdb0562ab743eb0eb83e91b7b
Jeannie <fareldwianggara05@gmail.com>
2026-06-02 14:58:47 +0000

    Revert "gpu/mali: soft-stop ticks 1->3 (100ms->300ms)"
    This reverts commit a1fefd34e40f029a793c741d6478277d66ffdade.

--- drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mali_kbase_config_defaults.h
+++ drivers/gpu/mediatek/gpu_mali/mali_valhall/mali-r32p1/drivers/gpu/arm/midgard/mali_kbase_config_defaults.h
@@ -124,12 +124,12 @@ enum {
  * This defines the time-slice for a job (which may be different from that of a
  * context)
  */
-#define DEFAULT_JS_SOFT_STOP_TICKS       (3) /* KernelBumi: 300ms, less stutter */
+#define DEFAULT_JS_SOFT_STOP_TICKS       (1) /* 100ms-200ms */
 
 /**
  * Default minimum number of scheduling ticks before CL jobs are soft-stopped.
  */
-#define DEFAULT_JS_SOFT_STOP_TICKS_CL    (3) /* KernelBumi: 300ms */
+#define DEFAULT_JS_SOFT_STOP_TICKS_CL    (1) /* 100ms-200ms */
 
 /**
  * Default minimum number of scheduling ticks before jobs are hard-stopped
