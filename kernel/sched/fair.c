2d4064da05e5c38b67a6a2181ce0c4ab2953cd35
Jeannie <fareldwianggara05@gmail.com>
2026-06-07 11:38:33 +0000

    Revert "sched/fair: 5.11 clamp task_util_est to SCHED_CAPACITY_SCALE"
    This reverts commit 5e21ac4b7647d0f3cdcf8e3a4a2c5d0fdf98f81f.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3761,9 +3761,7 @@ static inline unsigned long _task_util_est(struct task_struct *p)
 
 unsigned long task_util_est(struct task_struct *p)
 {
-	/* KernelBumi/5.11: clamp util_est to cpu capacity scale */
-	return min_t(unsigned long, max(task_util(p), _task_util_est(p)),
-		     SCHED_CAPACITY_SCALE);
+	return max(task_util(p), _task_util_est(p));
 }
 
 #ifdef CONFIG_UCLAMP_TASK
