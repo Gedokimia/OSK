5d5c2d6ef119181f6f130febfb81c7afc953e48d
Jeannie <fareldwianggara05@gmail.com>
2026-09-17 05:53:50 +0000

    Fix Runtime issue with the audio.

--- sound/soc/mediatek/mt6768/mt6768-afe-pcm.c
+++ sound/soc/mediatek/mt6768/mt6768-afe-pcm.c
@@ -45,7 +45,7 @@ static const struct snd_pcm_hardware mt6768_afe_hardware = {
 		    SNDRV_PCM_FMTBIT_S32_LE),
 	.period_bytes_min = 64,
 	.period_bytes_max = 4 * 48 * 1024,
-	.periods_min = 4,
+	.periods_min = 2,
 	.periods_max = 256,
 	.buffer_bytes_max = 8 * 48 * 1024,
 	.fifo_size = 0,
